627ab1fd316e81884531535063e1f109e33a7536
siheer <182635383+siheer@users.noreply.github.com>
2025-03-11 17:42:13 +0100

    add assets, apply first styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FruitListComponent } from './fruit-list/fruit-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FruitListComponent }
+];
