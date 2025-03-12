4c07a8c1ec8d489da516740dc97f6343953c905b
siheer <182635383+siheer@users.noreply.github.com>
2025-03-12 22:45:19 +0100

    add fruit list content and style

--- src/app/fruit-list/fruit-list.component.ts
+++ src/app/fruit-list/fruit-list.component.ts
@@ -8,9 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './fruit-list.component.scss'
 })
 export class FruitListComponent {
-
-
-
   fruitlist = [
     {
       name: "Apfel",
@@ -18,7 +15,7 @@ export class FruitListComponent {
       description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
       genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
       stars: 2.3,
-      reviews: [{ name: "Kevin W.", text: "ist lecker" }, { name: "Arne P.", text: "nicht so meins" }],
+      reviews: [],
     },
     {
       name: "Banane",
